364d1f8b912e0e365e5e395018558766342c93ac
CaptainChristmas <128335884+CaptainChristmas@users.noreply.github.com>
2025-04-21 14:17:38 +0800

    Add files via upload

--- test_benchmark.py
+++ test_benchmark.py
@@ -22,8 +22,13 @@ opt = parser.parse_args()
 UPSCALE_FACTOR = opt.upscale_factor
 MODEL_NAME = opt.model_name
 
-results = {'Set5': {'psnr': [], 'ssim': []}, 'Set14': {'psnr': [], 'ssim': []}, 'BSD100': {'psnr': [], 'ssim': []},
-           'Urban100': {'psnr': [], 'ssim': []}, 'SunHays80': {'psnr': [], 'ssim': [], 'lpips': []}}
+results = {
+    'Set5': {'psnr': [], 'ssim': [], 'lpips': []}, 
+    # 'Set14': {'psnr': [], 'ssim': [], 'lpips': []}, 
+    # 'BSD100': {'psnr': [], 'ssim': [], 'lpips': []},
+    # 'Urban100': {'psnr': [], 'ssim': [], 'lpips': []}, 
+    # 'SunHays80': {'psnr': [], 'ssim': [], 'lpips': []}
+          }
 
 model = Generator(UPSCALE_FACTOR).eval()
 if torch.cuda.is_available():
@@ -31,13 +36,18 @@ if torch.cuda.is_available():
 model.load_state_dict(torch.load('epochs/' + MODEL_NAME))
 
 test_set = TestDatasetFromFolder('data/test', upscale_factor=UPSCALE_FACTOR)
-test_loader = DataLoader(dataset=test_set, num_workers=4, batch_size=1, shuffle=False)
+test_loader = DataLoader(dataset=test_set, num_workers=8, batch_size=1, shuffle=False)
 test_bar = tqdm(test_loader, desc='[testing benchmark datasets]')
 
 out_path = 'benchmark_results/SRF_' + str(UPSCALE_FACTOR) + '/'
 if not os.path.exists(out_path):
     os.makedirs(out_path)
-
+    
+# 初始化 lpips_loss
+lpips_loss = lpips.LPIPS(net='vgg')
+if torch.cuda.is_available():
+    lpips_loss = lpips_loss.cuda()
+    
 for image_name, lr_image, hr_restore_img, hr_image in test_bar:
     image_name = image_name[0]
     lr_image = Variable(lr_image, volatile=True)
@@ -56,7 +66,7 @@ for image_name, lr_image, hr_restore_img, hr_image in test_bar:
         [display_transform()(hr_restore_img.squeeze(0)), display_transform()(hr_image.data.cpu().squeeze(0)),
          display_transform()(sr_image.data.cpu().squeeze(0))])
     image = utils.make_grid(test_images, nrow=3, padding=5)
-    utils.save_image(image, out_path + image_name.split('.')[0] + '_psnr_%.4f_ssim_%.4f.' % (psnr, ssim) +
+    utils.save_image(image, out_path + image_name.split('.')[0] + '_psnr_%.4f_ssim_%.4f_lpips_%.4f.' % (psnr, ssim, lpips_score) +
                      image_name.split('.')[-1], padding=5)
 
     # save psnr\ssim
@@ -65,7 +75,7 @@ for image_name, lr_image, hr_restore_img, hr_image in test_bar:
     results[image_name.split('_')[0]]['lpips'].append(lpips_score)
 
 out_path = 'statistics/'
-saved_results = {'psnr': [], 'ssim': []}
+saved_results = {'psnr': [], 'ssim': [], 'lpips': []}
 for item in results.values():
     psnr = np.array(item['psnr'])
     ssim = np.array(item['ssim'])
